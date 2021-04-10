338f254012e185c641f83db3d51e5cdcd33e9265
Eduardo Alveo <javialveo7@gmail.com>
2021-04-10 15:33:43 -0500

    Paleta de colores para la web, actualizado.

--- js/javialveo.js
+++ js/javialveo.js
@@ -1,13 +1,13 @@
 /******************************
  * File: javialveo.js         *
  * Author: Eduardo Alveo      *
- * Last Modified: 02-Jan-2021 *
+ * Last Modified: 10-Apr-2021 *
  ******************************/
 
 function viewMenu() {
   const containerMenu = document.querySelector("#menuWeb");
   
-  containerMenu.classList.toggle("menu--hide");
+  containerMenu.classList.toggle("navbar--hide");
 }
 
 function main() {
